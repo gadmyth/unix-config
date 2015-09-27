51422239bee0218788c78160041c1d585bd7f13d
gadmyth <gadmyth@gmail.com>
2015-09-27 23:32:39 +0800

    fix bug: load abbrev-config failed for the objc-mode-abbrev-table undefined

--- emacs/abbrev-config.el
+++ emacs/abbrev-config.el
@@ -3,14 +3,32 @@
 (define-key evil-insert-state-map (kbd "TAB") 'expand-abbrev)
 
 ;; for objc-mode
-(define-abbrev objc-mode-abbrev-table "ai" "allocinit")
-(define-abbrev objc-mode-abbrev-table "cd" "class-def")
-(define-abbrev objc-mode-abbrev-table "fd" "func-def")
-(define-abbrev objc-mode-abbrev-table "gfd" "gfunc-def")
-(define-abbrev objc-mode-abbrev-table "bi" "block-impl")
-(define-abbrev objc-mode-abbrev-table "bi" "block-impl")
-(define-abbrev objc-mode-abbrev-table "bp" "block-parameter")
-(define-abbrev objc-mode-abbrev-table "fb" "func-bracket")
+(add-hook
+ 'objc-mode-hook
+ (lambda ()
+   (progn
+     (mapc
+      (lambda (pair)
+        (define-abbrev objc-mode-abbrev-table (car pair) (cdr pair)))
+      '(("cd" . "class-def")
+        ("ced" . "class-ext-def")
+        ("ci" . "class-impl")
+        ("ced" . "class-ext-def")
+        ("cei" . "class-ext-impl")
+        ("ai" . "allocinit")
+        ("cd" . "class-def")
+        ("fd" . "func-def")
+        ("gfd" . "gfunc-def")
+        ("bd" . "block-declare")
+        ("bi" . "block-impl")
+        ("b1i" . "block1-impl")
+        ("bp" . "block-parameter")
+        ("ifb" . "if-block")
+        ("ieb" . "ifelse-block")
+        ("fore" . "for-enum")
+        ("fb" . "func-bracket")
+        )))))
 
+(setq save-abbrevs nil)
 
 (provide 'abbrev-config)
