6bb1092f529e1074fb2234e68f6fe47050b6da5a
gadmyth <gadmyth@gmail.com>
2016-03-05 22:12:52 +0800

    switch-buffer时, buffer不存在需要提示不存在

--- emacs/files-config.el
+++ emacs/files-config.el
@@ -7,10 +7,13 @@
 
 (defun switch-buffer (name)
   (interactive (list (read-string "Switch buffer(*scratch*): " nil nil "*scratch*")))
-  (let ((buffer (get-buffer-create name)))
-    (switch-to-buffer buffer)
-    (funcall (and initial-major-mode))
-    (setq buffer-offer-save t)))
+  (let ((buffer (get-buffer name)))
+    (if (buffer-live-p buffer)
+        (progn
+          (switch-to-buffer buffer)
+          (funcall (and initial-major-mode))
+          (setq buffer-offer-save t))
+      (message (format "The buffer %s doesn't exist!")))))
 
 (global-set-key (kbd "<f8>") 'new-buffer)
 (global-set-key (kbd "<f9>") 'switch-buffer)
