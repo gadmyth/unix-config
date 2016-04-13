a86320c27e256b05eb7eed21796b6629da841f57
gadmyth <gadmyth@gmail.com>
2016-04-13 23:40:29 +0800

    add make link script

---
+++ make_link.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd ~
+ln -sf unix-config/.bash_alias
+ln -sf unix-config/.bashrc
+ln -sf unix-config/.emacs
+ln -sf unix-config/.emacs.frame.el
+ln -sf unix-config/.emacs.shell.el
+ln -sf unix-config/.gitconfig
+ln -sf unix-config/.gvimrc
+ln -sf unix-config/.profile
+ln -s .gvimrc .vimrc
+ln -sf unix-config/emacs
+ln -sf unix-config/emacs.init.el
+ln -sf unix-config/mybash_profile
