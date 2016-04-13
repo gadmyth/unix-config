cad8be93c814976cc63a60afbff0caa6c212bdd9
gadmyth <gadmyth@gmail.com>
2016-04-13 17:15:51 +0800

    Modify the org mode

--- emacs/org-config.el
+++ emacs/org-config.el
@@ -2,14 +2,12 @@
 (require 'org)
 
 ;;; todo-keywords
-(setq org-todo-keywords '((type "work(w)" "learn(s)" "play(l)" "|")
-                          (sequence "DOING(i)" "PENDING(p)" "TODO(t)" "|" "DONE(d!)" "ABORT(a@/!)")))
+(setq org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "CLOING(c)" "PENDING(p)" "PLAN(l)" "|" "DONE(d!)" "ABORT(a@/!)")))
 
-(setq org-todo-keyword-faces '(("work" . (:background "red" :foreground "white" :weight bold))
-                               ("learn" . (:background "white" :foreground "red" :weight bold))
-                               ("play" . (:background "white" :foreground "MediumBlue" :weight bold))
-                               ("PENDING" . (:background "LightGreen" :foreground "black" :weight bold))
+(setq org-todo-keyword-faces '(("PENDING" . (:background "LightGreen" :foreground "black" :weight bold))
+                               ("PLAN" . (:background "LightGray" :foreground "white" :weight bold))
                                ("DOING" . (:background "Green" :foreground "white" :weight bold))
+                               ("CLOSING" . (:background "Orange" :foreground "white" :weight bold))
                                ("TODO" . (:background "DarkOrange" :foreground "black" :weight bold))
                                ("DONE" . (:background "azure" :foreground "Darkgreen" :weight bold))
                                ("ABORT" . (:background "gray" :foreground "black"))))
