dfaf6667ac14440e69d10ab022a1b79fc52f070d
gadmyth <gadmyth@gmail.com>
2017-08-10 21:56:48 +0800

    add objc-rewrite-cpp function

--- emacs/smart-compile-config.el
+++ emacs/smart-compile-config.el
@@ -5,5 +5,14 @@
 (require 'smart-compile)
 (add-to-list 'smart-compile-alist '(objc-mode . "clang -fobjc-arc -framework Foundation -w %f && ./a.out"))
 
+(defun objc-rewrite-cpp ()
+  "."
+  (interactive)
+  (with-current-buffer (current-buffer)
+    (let* ((command (format "clang -fobjc-arc -framework Foundation -rewrite-objc -w %s" buffer-file-name))
+           (result (shell-command-to-string command)))
+      (message command)
+      (message result))))
+
 (provide 'smart-compile-config)
 ;;; smart-compile-config.el ends here
