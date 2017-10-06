a9bb8a49593608d1379b4e34d4082354f3edf131
gadmyth <gadmyth@gmail.com>
2017-10-06 22:35:49 +0800

    add encode-file-as-base64-uri

--- emacs/codec.el
+++ emacs/codec.el
@@ -20,5 +20,14 @@
   (let ((str (delete-and-extract-region (region-beginning) (region-end))))
     (insert (md5 str))))
 
+(defun encode-file-as-base64-uri (file)
+  "FILE is the png or jpeg to encode."
+  (interactive "ffile: ")
+  (let* ((buffer-content (with-temp-buffer (insert-file-contents file) (buffer-string)))
+         (base64-content (base64-encode-string buffer-content))
+         (uri (concat "data:" "image/png" ";base64," base64-content)))
+    (message "encode-file-as-base-64-uri: %s" uri)
+    uri))
+
 (provide 'codec)
 ;;; codec.el ends here
