a8da92612bc484acddd8c67c77c7e82e6241e9ae
gadmyth <gadmyth@gmail.com>
2017-11-05 21:01:54 +0800

    add a function new-snippet-with-region

--- emacs/yas-config.el
+++ emacs/yas-config.el
@@ -1,4 +1,9 @@
+;;; package --- yas-config.el
+;;; Commentary:
+;;; Code:
+
 (require 'yasnippet)
+
 (add-hook
  'yas-global-mode-hook
  (lambda ()
@@ -7,4 +12,14 @@
 
 (setq yas-indent-line 'fixed)
 
+(defun new-snippet-with-region ()
+  "."
+  (interactive)
+  (let ((content (buffer-substring-no-properties (region-beginning) (region-end))))
+    (yas-new-snippet)
+    (with-current-buffer yas-new-snippet-buffer-name
+      (goto-char (point-max))
+      (insert content))))
+
 (provide 'yas-config)
+;;; yas-config.el ends here
