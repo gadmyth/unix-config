601c2b2c133b1a4ffcdc4e1b80897b92f9f27809
gadmyth <gadmyth@gmail.com>
2018-02-07 23:23:31 +0800

    objc-rewrite-cpp -> iOS platform

--- emacs/smart-compile-config.el
+++ emacs/smart-compile-config.el
@@ -9,7 +9,22 @@
   "."
   (interactive)
   (with-current-buffer (current-buffer)
-    (let* ((command (format "clang -fobjc-arc -framework Foundation -rewrite-objc -mmacosx-version-min=10.7 -fobjc-runtime=macosx-10.7 -w %s" buffer-file-name))
+    ;;; -mmacosx-version-min=10.7 -fobjc-runtime=macosx-10.7
+    (let* ((ios-min-version "7.0")
+           (runtime-version "ios-7.0")
+           (sysroot "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk")
+           (framework-path (concatenate 'string sysroot "/System/Library/Frameworks"))
+           (library-path (concatenate 'string sysroot "/usr/lib"))
+           (library-header-path (concatenate 'string sysroot "/usr/include"))
+           (filename buffer-file-name)
+           (command (format "clang -fobjc-arc -arch armv7 -framework Foundation -framework UIKit -rewrite-objc -miphoneos-version-min=%s -fobjc-runtime=%s -isysroot=%s -I\"%s\" -L\"%s\" -F\"%s\" -w %s"
+                            ios-min-version
+                            runtime-version
+                            sysroot
+                            library-header-path
+                            library-path
+                            framework-path
+                            buffer-file-name))
            (result (shell-command-to-string command)))
       (message command)
       (message result))))
