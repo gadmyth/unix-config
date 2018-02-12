d03922c38c85042d8d1ba42030046e5fa8a46844
gadmyth <gadmyth@gmail.com>
2018-02-13 01:14:07 +0800

    modify eyebrowse's key-binding, eyebrowse's next-slot's logic is wrong

--- emacs/eyebrowse-config.el
+++ emacs/eyebrowse-config.el
@@ -24,14 +24,15 @@
   "."
   (interactive)
   (let* ((window-configs (eyebrowse--get 'window-configs))
+         (slot 0)
          (current-slot (eyebrowse--get 'current-slot))
          (current-tag (nth 2 (assoc current-slot window-configs)))
          (current-element (ivy-eyebrowse-config-string current-slot current-tag))
          (next-slot (1+ current-slot))
+         (next-slot (if (> next-slot (- (length window-configs) 1)) 0 next-slot))
          (next-tag (nth 2 (assoc next-slot window-configs)))
          (next-element (ivy-eyebrowse-config-string next-slot next-tag))
          (ivy-prompt (format "Select eyebrowse action (%s): " current-element))
-         (slot 0)
          (collections '()))
     (dolist (window-config window-configs)
       (let* ((tag (nth 2 window-config))
@@ -40,7 +41,7 @@
         (incf slot)))
     (message "collections: %S" collections)
     (ivy-read ivy-prompt (reverse collections)
-              :preselect next-element
+              :preselect current-element
               :action (lambda (element)
                         (let ((slot (ivy-eyebrowse-config-slot element)))
                           (eyebrowse-switch-to-window-config slot))))))
@@ -60,17 +61,17 @@
                         (list "create config" #'eyebrowse-create-window-config-with-tag)
                         (list "close config" #'eyebrowse-close-window-config)))
          (ivy-actions (mapcar #'car actions))
+         (window-configs (eyebrowse--get 'window-configs))
          (current-slot (eyebrowse--get 'current-slot))
          (current-tag (nth 2 (assoc current-slot window-configs)))
          (current-element (ivy-eyebrowse-config-string current-slot current-tag))
          (ivy-prompt (format "Select eyebrowse action (config %s): " current-element)))
+    (message "element: %s, current-slot: %S, current-tag: %S" current-element current-slot current-tag)
     (ivy-read ivy-prompt ivy-actions
               :action (lambda (action-string)
-                        (message "action: %s" action-string)
                         (let ((action (first (remove-if-not
                                               (lambda (ele)
                                                 (equal action-string (first ele))) actions))))
-                          (message "%S" (functionp (second action)))
                           (command-execute (second action)))))))
 
 (provide 'eyebrowse-config)
