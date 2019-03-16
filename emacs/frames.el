0fe880e76572fa0505643db961a0a3b67a9c40a9
gadmyth <gadmyth@gmail.com>
2019-03-16 17:28:13 +0800

    默认显示menu和toolbar

--- emacs/frames.el
+++ emacs/frames.el
@@ -50,10 +50,10 @@
       (set-suitable-frame-size-inner frame))))
 
 (global-set-key (kbd "C-x 3") (lambda () (interactive) (select-window (split-window-right))))
-(if (boundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (boundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (boundp 'blink-cursor-mode) (blink-cursor-mode -1))
-(if (boundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (boundp 'tool-bar-mode) (tool-bar-mode +tool-bar-mode+))
+(if (boundp 'menu-bar-mode) (menu-bar-mode +menu-bar-mode+))
+(if (boundp 'blink-cursor-mode) (blink-cursor-mode +blink-cursor-mode+))
+(if (boundp 'scroll-bar-mode) (scroll-bar-mode +scroll-bar-mode+))
 (put 'scroll-left 'disabled nil)
 
 (provide 'frames)
