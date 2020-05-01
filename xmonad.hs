826dd86c26ee7ff43fd1d42bd9af341f70feb6b1
gadmyth <gadmyth@gmail.com>
2020-05-01 18:07:54 +0800

    xmonad: add xfce4-panel, xfce4-terminal support

--- xmonad.hs
+++ xmonad.hs
@@ -7,28 +7,24 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 
 main = do
-     xmproc <- spawnPipe "xmobar"
      xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
 	  modMask = mod4Mask
+	  , terminal = "xfce4-terminal"
           , borderWidth = 2
           , focusedBorderColor = "#ee4000"
           , normalBorderColor = "#9acd32"
 	  , manageHook = manageDocks <+> manageHook defaultConfig
 	  , layoutHook = avoidStruts $ layoutHook defaultConfig
           , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
-	  , logHook = loghook xmproc
           , startupHook = startup
 	} `additionalKeys`
-	[ ((mod4Mask .|. shiftMask, xK_g), spawn "firefox")
+	[ ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
 	, ((mod4Mask .|. shiftMask, xK_v), spawn "gvim")
-	, ((mod4Mask .|. shiftMask, xK_e), spawn "emacs") ]
-
-loghook h = dynamicLogWithPP $ xmobarPP {
-	ppOutput = hPutStrLn h
-	, ppTitle = xmobarColor "green" "" . shorten 50
-	, ppHiddenNoWindows = xmobarColor "grey" ""
-}
+	, ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
+        ]
 
 startup :: X()
 startup = do
         spawn "xrandr --output LVDS1 --auto; xrandr --output VGA1 --auto --right-of LVDS1"
+        spawn "xfce4-panel -d"
+
