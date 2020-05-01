f95a8a9acc68adcd0e4bf906f5bc14d9a446c3d7
gadmyth <gadmyth@gmail.com>
2020-05-01 20:37:34 +0800

    xmonad: add screensaver

--- xmonad.hs
+++ xmonad.hs
@@ -8,24 +8,26 @@ import XMonad.Hooks.UrgencyHook
 
 main = do
      xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
-	  modMask = mod4Mask
-	  , terminal = "xfce4-terminal"
+          modMask = mod4Mask
+          , terminal = "xfce4-terminal"
           , borderWidth = 2
           , focusedBorderColor = "#ee4000"
           , normalBorderColor = "#9acd32"
-	  , manageHook = manageDocks <+> manageHook defaultConfig
-	  , layoutHook = avoidStruts $ layoutHook defaultConfig
+          , manageHook = manageDocks <+> manageHook defaultConfig
+          , layoutHook = avoidStruts $ layoutHook defaultConfig
           , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
           , startupHook = startup
-	} `additionalKeys`
-	[ ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
-	, ((mod4Mask .|. shiftMask, xK_v), spawn "gvim")
-	, ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
+        , ((mod4Mask .|. shiftMask, xK_v), spawn "gvim")
+        , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
+        , ((controlMask .|. mod1Mask, xK_Delete), spawn "xscreensaver-command -lock")
         ]
 
 startup :: X()
 startup = do
         spawn "xrandr --output LVDS1 --auto; xrandr --output VGA1 --auto --right-of LVDS1"
+        spawn "xscreensaver -no-splash"
         spawn "xfce4-panel -d"
         spawn "yong -d"
 
