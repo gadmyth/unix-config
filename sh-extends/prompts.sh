654bd14c435d1fe444ef36465ae09f525505549e
gadmyth <gadmyth@gmail.com>
2020-06-25 11:27:01 +0800

    fix bug: can't get ip under MacOS unix bash using route

--- sh-extends/prompts.sh
+++ sh-extends/prompts.sh
@@ -9,12 +9,10 @@ function prompt {
 
     if [ "$SHELL" = "/bin/bash" ]; then
         # change the PS1
-        # PS1="\e[0;31m\e[47m[\!|\u@\h][\$(totalsize.sh)][\w]\e[m\n"
-        local ip=$(ifconfig $(route -n | grep ^0.0.0.0 | awk '{print $NF}') | grep inet | grep -v inet6 | awk '{print $2}')
+        local ip=$(ifconfig $(route -n get default | grep interface | awk '{print $2}') | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
         PS1="\e[0;31m\e[47m[\!|\u@$ip][\$(date +%k:%M:%S)][\w]\e[m\n"
         # change the PS2
         PS2="continue-->"
-
         # change PS4
         PS4='$0.$LINENO+ '
     fi
