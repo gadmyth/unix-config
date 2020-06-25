6d009b2fed92de76a5f51c92820e1e8533757642
gadmyth <gadmyth@gmail.com>
2020-06-25 11:57:59 +0800

    fix bug: shell prompt ip can't get under linux

--- sh-extends/prompts.sh
+++ sh-extends/prompts.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function default-interface() {
+    os="$(uname -s)"
+    case "${os}" in
+        Linux*)  route -n | grep ^0.0.0.0 | awk '{print $NF}';;
+        Darwin*) route -n get default | grep interface | awk '{print $2}';;
+        *) ""
+    esac
+}
+
 function prompt {
 	case $TERM in xterm*|rxvt*)
 		TITLEBAR='';;
@@ -9,7 +18,7 @@ function prompt {
 
     if [ "$SHELL" = "/bin/bash" ]; then
         # change the PS1
-        local ip=$(ifconfig $(route -n get default | grep interface | awk '{print $2}') | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
+        local ip=$(ifconfig $(default-interface) | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
         PS1="\e[0;31m\e[47m[\!|\u@$ip][\$(date +%k:%M:%S)][\w]\e[m\n"
         # change the PS2
         PS2="continue-->"
