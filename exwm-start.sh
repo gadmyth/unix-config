dc14d30de8b7b2d6c24f65371ff181c881989eec
gadmyth <gadmyth@gmail.com>
2020-08-25 07:24:44 +0800

    exwm: add exwm-start.sh

---
+++ exwm-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# copied from /etc/X11/xinit/xinitrc-common
+userresources=$HOME/.Xresources
+usermodmap=$HOME/.Xmodmap
+
+sysresources=/etc/X11/Xresources
+sysmodmap=/etc/X11/Xmodmap
+
+# merge in defaults
+[ -r "$sysresources" ] && xrdb -nocpp -merge "$sysresources"
+[ -r "$userresources" ] && xrdb -merge "$userresources"
+
+emacs
