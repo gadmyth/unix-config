fa617d6c28040a261d6636a40c364b1fcc2d932c
gadmyth <gadmyth@gmail.com>
2021-07-21 10:34:34 +0800

    fix .Xresources file's path

--- make_link.sh
+++ make_link.sh
@@ -7,7 +7,7 @@ ln -sf unix-config/.gitconfig
 ln -sf unix-config/.gvimrc
 ln -sf unix-config/.profile
 ln -sf unix-config/.Xmodmap
-ln -sf unix-config/.Xresources
+ln -sf unix-config/.xmonad/.Xresources
 ln -sf unix-config/.tmux.conf
 rm -f .vimrc
 ln -s .gvimrc .vimrc
