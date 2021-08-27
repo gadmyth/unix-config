68142c026097df2d579a2e9e4f3f826d15e89494
gadmyth <gadmyth@gmail.com>
2021-08-27 08:59:57 +0800

    add mojo-weixin script

---
+++ perl/mojo-weixin.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use Mojo::Weixin;
+
+my ($host,$port,$post_api);
+
+$host = "0.0.0.0";
+$port = 3000;
+#$post_api = 'http://xxxx';
+
+my $client = Mojo::Weixin->new(log_level=>"info",http_debug=>0);
+$client->load("ShowMsg");
+$client->load("IRCShell", data=>{load_friend=>0});
+$client->load("ShowQRCodeInTerm");
+$client->load("Openwx",data=>{listen=>[{host=>$host,port=>$port}], post_api=>$post_api});
+$client->run();
