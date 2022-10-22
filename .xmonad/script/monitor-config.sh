689c643a3ad0a2896227562abe607f59a68e0f34
gadmyth <gadmyth@gmail.com>
2022-10-22 09:29:34 +0800

    xmonad: modify monitor-script.sh

--- .xmonad/script/monitor-config.sh
+++ .xmonad/script/monitor-config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-XMONAD_DEVICE="new-fedora"
 
-if [[ "$XMONAD_DEVICE" == "new-fedora" ]]; then
+if [[ "$(uname -n)" == "new-fedora" ]]; then
     xrandr --output DP-2 --primary --auto --output DP-1 --auto --scale 1.5x1.5 --rotate left --right-of DP-2
+elif [[ "$(uname -n)" == "old-fedora" ]]; then
+    xrandr --output VGA-1 --primary --auto --output DP-1 --auto --scale 0.7x0.7 --left-of VGA-1
 fi
