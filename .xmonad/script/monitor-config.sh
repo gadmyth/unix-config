611f60ae4cc5c50e7d4b7ec4150d96e6c73a4fea
gadmyth <gadmyth@gmail.com>
2022-12-26 16:25:40 +0800

    xmonad: update monitor-config.sh

--- .xmonad/script/monitor-config.sh
+++ .xmonad/script/monitor-config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+# 0.9999x0.9999 scale will fix mouse flashing 
 
 if [[ "$(uname -n)" == "new-fedora" ]]; then
-    xrandr --output DP-2 --primary --auto --output DP-1 --auto --scale 1.5x1.5 --rotate left --right-of DP-2
+    xrandr --output DP-2 --primary --auto --scale 0.9999x0.9999 --output DP-1 --auto --scale 1.5x1.5 --rotate left --right-of DP-2
 elif [[ "$(uname -n)" == "old-fedora" ]]; then
-    xrandr --output VGA-1 --primary --auto --output DP-1 --auto --scale 0.7x0.7 --left-of VGA-1
+    xrandr --output DP-1 --primary --auto --scale 0.7x0.7 --output VGA-1 --auto --scale 0.9999x0.9999 --right-of DP-1
 fi
