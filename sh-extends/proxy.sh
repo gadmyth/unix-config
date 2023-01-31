3df1eada23876062f8e8c6d1f98a096b7a44d10a
gadmyth <gadmyth@gmail.com>
2023-01-31 20:25:42 +0800

    sh-extends: proxy function seperate privoxy and socks5

--- sh-extends/proxy.sh
+++ sh-extends/proxy.sh
@@ -1,6 +1,11 @@
-function turn-on-proxy() {
+function turn-on-proxy-privoxy() {
     export https_proxy=127.0.0.1:8118
     export http_proxy=127.0.0.1:8118
+}
+
+function turn-on-proxy-socks5() {
+    export https_proxy=127.0.0.1:1080
+    export http_proxy=127.0.0.1:1080
 }
 
 function turn-off-proxy() {
