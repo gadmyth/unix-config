2acd52ae8db0c700a045e0fdf8b4e56c649ece71
gadmyth <gadmyth@gmail.com>
2023-12-28 22:37:54 +0800

    sh-extends: modify proxy.sh and ps's script

--- sh-extends/proxy.sh
+++ sh-extends/proxy.sh
@@ -1,6 +1,6 @@
 function turn-on-proxy-privoxy() {
-    export https_proxy=127.0.0.1:8118
-    export http_proxy=127.0.0.1:8118
+    export https_proxy=http://127.0.0.1:8118
+    export http_proxy=http://127.0.0.1:8118
 }
 
 function turn-on-proxy-socks5() {
